6985edfca5e7b44186299d3041918d655f2e191c
csndylim <54385174+csndylim@users.noreply.github.com>
2023-03-06 20:50:20 +0800

    complete base updated jira to elk code

--- logstash/pipeline/updated_issues_jira_to_elk.py
+++ logstash/pipeline/updated_issues_jira_to_elk.py
@@ -13,62 +13,36 @@ password = "jira"
 username_elastic = "elastic"
 password_elastic = "P@$$w0rd"
 
-# =============================================================================
-# Jira
-# =============================================================================
+# Get the current date
+current_date = str(datetime.date.today().strftime('%Y-%m-%d'))
 
-# jira = JIRA(options, basic_auth=(username, password))
+jira = JIRA(options, basic_auth=(username, password))
 
-# # specify the JQL query to search for issues
-# jql_query = 'project = TEST'
-
-# # get the total number of issues
-# total_issues = jira.search_issues(jql_query, maxResults=0, fields=None, expand=None, json_result=None).total
-
-
-# num_issues = 50
-# # Calculate the number of pages of issues to fetch
-# num_pages = (total_issues + num_issues - 1) // num_issues
-
-# # Fetch the issues from each page
-# issues = []
-# # Set the maximum number of issues to be retrieved
-# max_results = 100
-
-# # Get the issues
-# start_at = 0
-# issues = []
-# while True:
-#     results = jira.search_issues(jql_query, startAt=start_at, maxResults=max_results)
-#     if not results:
-#         break
-#     issues += results
-#     start_at += max_results
-
-# # Print the issues
-# for issue in issues:
-#     print(issue)
-
-
-# =============================================================================
-# Elasticsearch
-# =============================================================================
-# process the issues as needed
 es = Elasticsearch(
     [{'host': 'localhost', 'port': 9200, 'scheme' : 'http'}],
     http_auth=(username_elastic, password_elastic),
     verify_certs=True
 )
 
-# Get the current date
-current_date = str(datetime.date.today().strftime('%Y-%m-%d'))
 
+# =============================================================================
+# Get the issues that are updated
+# =============================================================================
+
+# specify the JQL query to search for issues
+jql_query = "project = TEST AND updated >= " + current_date
+
+
+issues = jira.search_issues(jql_query)
+issue_keys = [issue.key for issue in issues]
+
+
+# =============================================================================
+# Delete the issues on elasticsearch
+# =============================================================================
 # Define the index and type of the documents to be deleted
 index = 'jiratestv14-' + current_date
 doc_type = "_doc"
-
-
-issue_keys = ["TEST-1", "TEST-2", "TEST-3"]
 
 # get list of document ID from issue_keys on elasticseach
 doc_ids = []
@@ -83,7 +57,6 @@ for issue_key in issue_keys:
 
     res = es.search(index=index, body=search_body)
     doc_ids.append(res["hits"]["hits"][0]["_id"])
-
 
 # Define a list of Elasticsearch delete requests for each document
 delete_requests = [
@@ -100,6 +73,75 @@ bulk(es, delete_requests)
 
 # Delete just one document
 # es.delete(index='my_index', doc_type='_doc', id='1')
+
+
+
+# =============================================================================
+# Add in the new issues on elasticsearch
+# =============================================================================
+# # Set the maximum number of issues to be retrieved
+max_results = 100
+
+# Define the JQL query to get the issues
+jql_query = "project = TEST AND updated >= " + current_date
+
+# Get the issues
+start_at = 0
+issues = []
+while True:
+    results = jira.search_issues(jql_query, startAt=start_at, maxResults=max_results)
+    if not results:
+        break
+    issues += results
+    start_at += max_results
+
+    # Index the Jira issues in Elasticsearch
+    for issue in issues:
+        issue_dict = {}
+  
+        # Extract all fields from Jira issue
+        for field_name in issue.raw['fields']:
+            issue_dict[field_name] = issue.raw['fields'][field_name]
+  
+        # Extract comments
+        try:
+            comments = []
+            for comment in issue.fields.comment.comments:
+                comments.append(comment.body)
+            issue_dict['comments'] = comments
+        except AttributeError:
+            issue_dict['comments'] = []
+  
+        # Extract worklogs
+        try:
+            worklogs = []
+            for worklog in issue.fields.worklog.worklogs:
+                worklogs.append({
+                    'author': worklog.author.displayName,
+                    'timeSpent': worklog.timeSpent,
+                    'created': worklog.created
+                })
+            issue_dict['worklogs'] = worklogs
+        except AttributeError:
+            issue_dict['worklogs'] = []
+  
+        issue_dict['key'] = issue.key
+        # issue_dict['summary'] = issue.fields.summary
+        # issue_dict['description'] = issue.fields.description
+        # issue_dict['assignee'] = issue.fields.assignee.name if issue.fields.assignee else None
+        # issue_dict['reporter'] = issue.fields.reporter.name if issue.fields.reporter else None
+        # issue_dict['updated_date'] = issue.fields.updated if issue.fields.updated else None
+        # issue_dict['created_date'] = issue.fields.created if issue.fields.created else None
+  
+        issue_dict['timestamp']  = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+  
+        # Get the current date
+        current_date = str(datetime.date.today().strftime('%Y-%m-%d'))
+  
+        # Index the Jira issues in Elasticsearch using the current date
+        es.index(index='jiratestv14-' + current_date, body=json.dumps(issue_dict))
+        
+        print (issue_dict)
 
 
 
